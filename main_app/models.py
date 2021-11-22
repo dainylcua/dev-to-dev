16624bf05f7bb8500f80711698b49ae0c1303b53
fredd <fredd.oropeza@gmail.com>
2021-11-22 16:50:06 -0500

    transfers work to fredd branch

--- main_app/models.py
+++ main_app/models.py
@@ -2,10 +2,26 @@ from django.db import models
 from django.db.models.deletion import CASCADE
 from django.urls import reverse
 from django.contrib.auth.models import User
+from django.db.models.fields.related import ManyToManyField 
 
-class Topic(models.Model):
-    id = models.ForeignKey(User, on_delete=models.CASCADE)
+class Comment(models.Model):
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # post = models.ForeignKey(User, on_delete=models.CASCADE)
+    topic_id = models.CharField(max_length=50)
     author_id = models.CharField(max_length=50)
+    content = models.TextField(max_length=250)
+    score = models.IntegerField()
+    create_date = models.DateField('Created Date')
+    
+    def __str__(self):
+        return f'{self.get_meal_display()} on {self.date}'
+
+    # class Meta:
+    #     ordering = ('-date',)
+        
+class Post(models.Model):
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # comments = ManyToManyField(Comment)
     title = models.CharField(max_length=50)
     description = models.TextField(max_length=250)
     score = models.IntegerField()
@@ -29,18 +45,5 @@ class Topic(models.Model):
 #         return reverse('detail', kwargs={'pk': self.id})
 
 
-class Comments(models.Model):
-    id = models.ForeignKey(User, on_delete=models.CASCADE)
-    topic_id = models.CharField(max_length=50)
-    author_id = models.CharField(max_length=50)
-    content = models.TextField(max_length=250)
-    score = models.IntegerField()
-    create_date = models.DateField('Created Date')
-    
-    def __str__(self):
-        return f'{self.get_meal_display()} on {self.date}'
-
-    class Meta:
-        ordering = ('-date',)
 
 # Create your models here.

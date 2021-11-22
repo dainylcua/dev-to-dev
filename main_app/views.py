2010589903fe633584dbdc92ed8de0b5c9a9bc94
dainylcua <dainylcua@gmail.com>
2021-11-22 15:05:15 -0600

    Switching branches

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Topic, Comment
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
 
 # Create your views here.

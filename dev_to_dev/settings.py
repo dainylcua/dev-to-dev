029b81818d017a8b5b4a35bcc8c1a545b23b42a3
apareja <alexis.pareja@hotmail.com>
2021-11-29 10:21:37 -0500

    sqlite setting

--- dev_to_dev/settings.py
+++ dev_to_dev/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'dev_to_dev.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dev_to_dev',
-        'USER': 'admin',
-        'PASSWORD': 'abc1234',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 

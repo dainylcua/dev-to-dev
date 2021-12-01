bf38ebff0d193de1b02cfc14cc09140099198408
dainylcua <dainylcua@gmail.com>
2021-12-01 09:13:06 -0600

    Adds redirects

--- dev_to_dev/settings.py
+++ dev_to_dev/settings.py
@@ -119,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL = '/topics/'
+LOGOUT_REDIRECT_URL = '/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
